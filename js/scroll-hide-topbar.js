46e70f21807b0d14f19c97dd914e5ca3e253ad85
pranav0963 <94529251+pranav0963@users.noreply.github.com>
2024-02-07 01:27:50 +0530

    version 1

--- js/scroll-hide-topbar.js
+++ js/scroll-hide-topbar.js
@@ -9,6 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
 document.addEventListener("DOMContentLoaded", function () {
     var header = document.getElementById("header");
 
@@ -20,3 +21,40 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    let logo1 = document.querySelector(".hero");
+    let logo2 = document.querySelector(".scroll-hero");
+    logo2.classList.add("hidden")
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 100) {
+            logo1.classList.add("hidden");
+            logo2.classList.remove("hidden");
+        } else {
+            logo2.classList.add("hidden");
+            logo1.classList.remove("hidden");
+        }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    let links = document.querySelectorAll(".colorednav");
+
+    window.addEventListener("scroll", function () {
+        let y = window.scrollY || window.pageYOffset;
+
+        if (y > 100) {
+            links.forEach(function (link) {
+                link.style.color = "#001549";
+            });
+        } else {
+            links.forEach(function (link) {
+                link.style.color = "#666666";
+            });
+        }
+    });
+});
+
+
+
